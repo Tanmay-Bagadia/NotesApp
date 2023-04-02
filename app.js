ff45a480d626b6a9c4a927b84653ca1514879262
Tanmay-Bagadia <tanmaybagadia13@gmail.com>
2023-04-02 21:48:49 +0530

    Basic ui done

---
+++ app.js
@@ -0,0 +1,9 @@
+// Get DOM Elements
+
+let notesInput = document.getElementById("notes-input");
+let addBtn = document.getElementsByClassName("addBtn")[0];
+
+function createNotes() {
+  // getting value of notes from the text area
+  let note = notesInput.value;
+}
