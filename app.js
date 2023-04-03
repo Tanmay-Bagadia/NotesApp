2d344882f06b54365b8e8f95229fbad62da2b90e
Tanmay-Bagadia <tanmaybagadia13@gmail.com>
2023-04-03 18:46:26 +0530

    Delete function done

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Get DOM Elements
 
-let notesInput = document.getElementById("notes-input").value; //   getting value from text area;
-console.log(notesInput);
+let notesInput = document.getElementById("notes-input"); //   getting value from text area;
+console.log(notesInput.value);
 let note = document.getElementsByClassName("notes-area")[0];
 let addBtn = document.getElementsByClassName("addBtn")[0];
-let title = document.getElementsByClassName("title-input")[0].value; //getting title value from the input
+let title = document.getElementsByClassName("title-input")[0]; //getting title value from the input
 let notesContainer = document.getElementsByClassName("notes-cont")[0];
 
 function createNote() {
@@ -12,17 +12,29 @@ function createNote() {
   let eDiv = document.createElement("div");
   let eHeading = document.createElement("h2");
   let notePara = document.createElement("p");
+  let delBtn = document.createElement("button");
 
   //   give class name
   eDiv.classList.add("notes");
   eHeading.classList.add("heading");
   notePara.classList.add("notePara");
+  delBtn.classList.add("delBtn");
 
   //   append elements
   notesContainer.append(eDiv);
   eDiv.append(eHeading);
   eDiv.append(notePara);
+  eDiv.append(delBtn);
 
-  notePara.innerHTML = notesInput;
-  eHeading.innerHTML = title;
+  notePara.innerHTML = `Note: ${notesInput.value}`;
+  notesInput.value = "";
+
+  eHeading.innerHTML = `Title: ${title.value}`;
+  title.value = "";
+
+  delBtn.innerHTML = "Delete";
+  delBtn.addEventListener("click", () => {
+    eDiv.remove();
+    alert("Your note had been deleted.");
+  });
 }
